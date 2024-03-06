bce863c139a6f352efbb6d79420400ef93b79493
Jodi Diffey <119941206+purpleysound@users.noreply.github.com>
2024-03-06 19:20:56 +0000

    fixed leaderboard and added 2nd "letter" leaderboard

--- leaderboard_handler.py
+++ leaderboard_handler.py
@@ -3,13 +3,15 @@ from utils import sort_dictionary
 import hashlib
 
 def get_digest(leaderboard):
-    return hashlib.sha256(json.dumps(leaderboard["leaderboard"]).encode()).hexdigest()
+    return hashlib.sha256(
+        f"{json.dumps(leaderboard["words"])},{json.dumps(leaderboard["letters"])}"
+        .encode()).hexdigest()
 
-EMPTY_HASH = get_digest({"leaderboard": {}})
+EMPTY_HASH = get_digest({"words": {}, "letters": {}})
 
 def create_leaderboard():
     with open("leaderboard.json", "w") as f:
-        json.dump({"words": {}, "digest": EMPTY_HASH}, f, indent=4)
+        json.dump({"words": {}, "letters": {}, "digest": EMPTY_HASH}, f, indent=4)
 
 def open_leaderboard():
     try:
@@ -26,14 +28,26 @@ def open_leaderboard():
         raise RuntimeError("Leaderboard digest doesn't match, file has either been tampered with or an error has occured")
     
     
-def update_leaderboard(name, score):
-    leaderboard = open_leaderboard()
-    leaderboard["leaderboard"][name] = score
-    leaderboard["leaderboard"] = sort_dictionary(leaderboard["leaderboard"], key=lambda x: x[1], reverse=True)
-    leaderboard["digest"] = get_digest(leaderboard)
-    with open("leaderboard.json", "w") as f:
-        json.dump(leaderboard, f, indent=4)
-    return leaderboard
+def update_leaderboard(name: str, words: int, letters: int) -> bool:
+    """Updates leaderboard json file with new score if it is higher than the previous score
+    Returns True if a new high score was set, False otherwise"""
+    updated = False
+    leaderboard: dict[str, dict | str] = open_leaderboard()
+    previous = leaderboard["words"].get(name, -1)
+    if words > previous:
+        leaderboard["words"][name] = words
+        leaderboard["words"] = sort_dictionary(leaderboard["words"], key=lambda x: x[1], reverse=True)
+        updated = True
+    previous = leaderboard["letters"].get(name, -1)
+    if letters > previous:
+        leaderboard["letters"][name] = letters
+        leaderboard["letters"] = sort_dictionary(leaderboard["letters"], key=lambda x: x[1], reverse=True)
+        updated = True
+    if updated:
+        leaderboard["digest"] = get_digest(leaderboard)
+        with open("leaderboard.json", "w") as f:
+            json.dump(leaderboard, f, indent=4)
+    return updated 
     
 if __name__ == "__main__":
     print(open_leaderboard())
